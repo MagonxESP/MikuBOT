71011ab05581c912f89c301d4601b9e2bcdcfb76
MagonxESP <janma.360@gmail.com>
2019-08-13 22:28:49 +0200

    start irc client thread

--- MikuBOT/__init__.py
+++ MikuBOT/__init__.py
@@ -1,7 +1,22 @@
-from MikuBOT.mikubot import MikuBOT
+from MikuBOT.mikubot import MikuBOT, MikuBOTIRC
 from MikuBOT import settings
 import MikuBOT.entities as entities
+from threading import Thread
 
+# Start irc client thread
+irc_bot = MikuBOTIRC()
+irc_bot.connect(
+    settings.IRC_HOST,
+    settings.IRC_PORT,
+    settings.IRC_USERNAME,
+    username=settings.IRC_USERNAME,
+    password=settings.IRC_PASSWORD
+)
+
+irc_thread = Thread(target=irc_bot.start)
+irc_thread.start()
+
+# Connect to mysql database and start discord bot
 entities.db.bind(
     provider='mysql',
     host=settings.MYSQL_HOST,
