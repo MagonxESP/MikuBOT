f63f80c2094a7adbbc8f49ecbb8032ef0cef2a21
MagonxESP <janma.360@gmail.com>
2019-08-14 22:21:15 +0200

    consts bot instances

--- MikuBOT/__init__.py
+++ MikuBOT/__init__.py
@@ -1,31 +1,6 @@
-from MikuBOT.mikubot import MikuBOT, MikuBOTIRC
-from MikuBOT import settings
-import MikuBOT.entities as entities
-from threading import Thread
+from MikuBOT.mikubot import MikuBOT
+from MikuBOT.ircbot import MikuBOTIRC
 
-# Start irc client thread
-irc_bot = MikuBOTIRC()
-irc_bot.connect(
-    settings.IRC_HOST,
-    settings.IRC_PORT,
-    settings.IRC_USERNAME,
-    username=settings.IRC_USERNAME,
-    password=settings.IRC_PASSWORD
-)
-
-irc_thread = Thread(target=irc_bot.start)
-irc_thread.start()
-
-# Connect to mysql database and start discord bot
-entities.db.bind(
-    provider='mysql',
-    host=settings.MYSQL_HOST,
-    user=settings.MYSQL_USER,
-    passwd=settings.MYSQL_PASSWORD,
-    db=settings.MYSQL_DATABASE
-)
-
-entities.db.generate_mapping(create_tables=True)
-
-bot = MikuBOT()
-bot.run()
+# create bots instances
+DISCORD_BOT = MikuBOT()
+IRC_BOT = MikuBOTIRC()
