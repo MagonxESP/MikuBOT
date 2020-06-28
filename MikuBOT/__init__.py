1b36ba86f5f86cae6e44fced337f902d6a0f886e
MagonxESP <janma.360@gmail.com>
2020-06-28 18:36:12 +0200

    logs directory

--- MikuBOT/__init__.py
+++ MikuBOT/__init__.py
@@ -1,16 +1,22 @@
 from MikuBOT.mikubot import MikuBOT
 from MikuBOT.ircbot import MikuBOTIRC
-from os.path import join, dirname
+import os
 import logging
 import setproctitle
 
 # set process title
 setproctitle.setproctitle('MikuBOT')
 
+root_dir = os.path.join(os.path.dirname(__file__), '..')
+logs_dir = os.path.join(root_dir, 'logs')
+
+if os.path.exists(logs_dir) is False:
+    os.mkdir(logs_dir)
+
 # configure and get logger
 logging.basicConfig(
     level=logging.INFO,
-    filename=join(dirname(__file__), '../app.log'),
+    filename=os.path.join(logs_dir, 'app.log'),
     filemode='a',
     format='%(asctime)s :: %(levelname)s :: %(message)s',
     datefmt='%d-%b-%y %H:%M:%S'
